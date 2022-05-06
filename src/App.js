f597f782d09a024e269e868746f688758117243b
Mansi Ladkar <ladkarmansi@gmail.com>
2022-05-06 19:27:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import "./App.css";
+import React from "react";
+import Header from "./Components/Header";
+import { Formik, Form } from "Formik";
+import * as Yup from "yup";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Grid } from "@material-ui/core";
 
-function App() {
-  return <div>hi</div>;
-}
+const useStyles = makeStyles((theme) => ({
+  formWrapper: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(8),
+  },
+}));
+
+const INITIAL_FORM_STATE = {};
+
+const FORM_VALIDATION = Yup.object().shape({});
+
+const App = () => {
+  const classes = useStyle();
+
+  return (
+    <Grid Container>
+      <Grid item xs={12}>
+        <Header />
+      </Grid>
+      <Grid item xs={12}>
+        <Container maxWidth="md">
+          <div className={classes.formWrapper}>
+            <Formik
+              initialValues={{ ...INITIAL_FORM_STATE }}
+              validationSchema={FORM_VALIDATION}
+              onSubmit={(values) => {
+                console.log(values);
+              }}
+            >
+              <Form>
+                <Grid container spacing={2}></Grid>
+              </Form>
+            </Formik>
+          </div>
+        </Container>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default App;
